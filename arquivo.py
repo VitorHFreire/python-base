e3e40451564585fcf6c39ac3624cf1e3fd397887
Vitor Hugo <vitorhfreire@outlook.com>
2022-05-11 21:13:20 -0300

    Variável msg

--- arquivo.py
+++ arquivo.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
-print("Hello, World")
+""" Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a
+correspondende.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada, ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 arquivo.py
+
+"""
+msg = "Hello, World"
+
+print(msg)
+
